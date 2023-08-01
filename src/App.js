a2fdbb258472739b26636fc430ef3474971c6023
MuzafferBYRKTR <muzafferbayraktar51@gmail.com>
2023-08-01 21:06:29 +0300

    First React Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logos from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="logo192.png" />
+      <img src={logos} />
+    </div>
+  );
+}
+
+export default App;
